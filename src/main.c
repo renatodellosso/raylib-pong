0e7c6233d909fa9f4b76007440f2661252d4d23a
Renato Dell'Osso <rddellosso4@gmail.com>
2026-02-01 22:31:05 -0500

    first commit

---
+++ src/main.c
@@ -0,0 +1,125 @@
+/*
+Raylib example file.
+This is an example main file for a simple raylib project.
+Use this as a starting point or replace it with your code.
+
+by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
+
+*/
+#include <stdio.h>
+#include "raylib.h"
+
+#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+
+float min(float x, float y) {
+	if (x < y)
+		return x;
+	return y;
+}
+
+float max(float x, float y) {
+	if (x > y)
+		return x;
+	return y;
+}
+
+const Vector2 paddleSize = {
+	10, 250
+};
+
+const float BALL_RADIUS = 10;
+const float PADDLE_SPEED = 250;
+
+int main ()
+{
+	// Tell the window to use vsync and work on high DPI displays
+	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+
+
+	// Create the window and OpenGL context
+	InitWindow(100, 100, "Pong");
+	SetWindowPosition(GetScreenWidth() / 4, GetScreenHeight() / 4);
+
+	int monitor = GetCurrentMonitor();
+	int width = GetMonitorWidth(monitor) * 4 / 5, height = GetMonitorHeight(monitor) * 4 / 5;
+
+	char str[20];
+
+	SetWindowSize(width, height);
+
+	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	SearchAndSetResourceDir("resources");
+
+	Rectangle leftPaddle = {
+		10, height / 2 - paddleSize.y / 2,
+		paddleSize.x, paddleSize.y
+	};
+
+	Rectangle rightPaddle = {
+		width - paddleSize.x - 10, height / 2 - paddleSize.y / 2,
+		paddleSize.x, paddleSize.y
+	};
+
+	Vector2 ballPos = {
+		width / 2, height / 2
+	};
+	Vector2 ballVelocity = {
+		150, 150
+	};
+
+	int score = 0;
+	
+	// game loop
+	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
+	{
+		// drawing
+		BeginDrawing();
+
+		// Setup the back buffer for drawing (clear color and depth buffers)
+		ClearBackground(BLACK);
+
+		// draw some text using the default font
+
+		DrawText(TextFormat("Score: %i", score), width / 2 - 50, 0, 20, WHITE);
+
+		DrawRectangleRec(leftPaddle, WHITE);
+		DrawRectangleRec(rightPaddle, WHITE);
+
+		DrawCircleV(ballPos, BALL_RADIUS, YELLOW);
+
+		float deltaTime = GetFrameTime();
+		Vector2 scaledVelocity = {
+			ballVelocity.x * deltaTime,
+			ballVelocity.y * deltaTime
+		};
+		ballPos.x += scaledVelocity.x;
+		ballPos.y += scaledVelocity.y;
+
+		if (IsKeyDown(KEY_S))
+			leftPaddle.y = min(leftPaddle.y + deltaTime * PADDLE_SPEED, height);
+		else if (IsKeyDown(KEY_W))
+			leftPaddle.y = max(leftPaddle.y - deltaTime * PADDLE_SPEED, 0);
+
+		if (IsKeyDown(KEY_DOWN))
+			rightPaddle.y = min(rightPaddle.y + deltaTime * PADDLE_SPEED, height);
+		else if (IsKeyDown(KEY_UP))
+			rightPaddle.y = max(rightPaddle.y - deltaTime * PADDLE_SPEED, 0);
+
+		if (CheckCollisionCircleRec(ballPos, BALL_RADIUS, leftPaddle) || CheckCollisionCircleRec(ballPos, BALL_RADIUS, rightPaddle)) {
+			ballVelocity.x *= -1.1;
+			score++;
+		}
+
+		if (ballPos.y > height || ballPos.y < 0) {
+			ballPos.y = max(min(ballPos.y, height), 0);
+			ballVelocity.y *= -1;
+		}
+		
+		// end the frame and get ready for the next one  (display frame, poll input, etc...)
+		EndDrawing();
+	}
+
+	// destroy the window and cleanup the OpenGL context
+	CloseWindow();
+	return 0;
+}
